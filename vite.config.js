9c3f681d49a2ef15d9639aa55ddc23f6f2e60867
Tommy <wittom1410@gmail.com>
2026-02-09 18:16:07 +0100

    port

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 30127, // Tutaj ustawiasz swój port
+    strictPort: true, // Jeśli port 3000 zajęty, Vite nie będzie szukał kolejnego wolnego
+  }
 })
