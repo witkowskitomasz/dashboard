74790dcd6025231b74c02e0dabe44a3bbcaeb064
Tommy <wittom1410@gmail.com>
2026-02-09 18:22:52 +0100

    error

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,8 @@ export default defineConfig({
   server: {
     port: 30127, // Tutaj ustawiasz swój port
     strictPort: true, // Jeśli port 3000 zajęty, Vite nie będzie szukał kolejnego wolnego
+    allowedHosts: [
+      'hanna127.mikrus.xyz' // Tutaj dodajesz swoją domenę z Mikrusa
+    ]
   }
 })
